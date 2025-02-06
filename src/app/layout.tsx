7d955aad49a1178e262a30d8d8d764fe032e8c36
StrigZ <mirgasan200@gmail.com>
2025-02-06 16:19:04 +0300

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Debug Realm",
+  description: "Browse and purchase games with ease!",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}

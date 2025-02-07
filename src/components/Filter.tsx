57d4fec6eb5658e2e28bed3005ac811cdf30ce40
StrigZ <mirgasan200@gmail.com>
2025-02-07 17:51:04 +0300

    Remove sidebar links

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -23,35 +23,27 @@ const filters: {
 type Props = { classNames?: string };
 export default function Filter({ classNames }: Props) {
   return (
-    <header
-      className={cn(
-        'flex items-center justify-between bg-slate-950 pb-4',
-        classNames,
-      )}
-    >
-      <h1 className="text-4xl font-bold">Top Selling</h1>
-      <div className="space-x-4">
-        <select
-          className="cursor-pointer rounded-lg border border-white bg-slate-950 p-4 text-white"
-          defaultValue="All Time"
-        >
-          {dateFilters.map(({ displayText }) => (
-            <option key={displayText} value={displayText}>
-              {displayText}
-            </option>
-          ))}
-        </select>
-        <select
-          className="cursor-pointer rounded-lg border border-white bg-slate-950 p-4 text-white"
-          defaultValue="metacritic"
-        >
-          {filters.map(({ displayText, value }) => (
-            <option key={value} value={value}>
-              {displayText}
-            </option>
-          ))}
-        </select>
-      </div>
+    <header className={cn('space-x-4 bg-slate-950 pb-4', classNames)}>
+      <select
+        className="cursor-pointer rounded-lg border border-white bg-slate-950 p-4 text-white"
+        defaultValue="All Time"
+      >
+        {dateFilters.map(({ displayText }) => (
+          <option key={displayText} value={displayText}>
+            {displayText}
+          </option>
+        ))}
+      </select>
+      <select
+        className="cursor-pointer rounded-lg border border-white bg-slate-950 p-4 text-white"
+        defaultValue="metacritic"
+      >
+        {filters.map(({ displayText, value }) => (
+          <option key={value} value={value}>
+            {displayText}
+          </option>
+        ))}
+      </select>
     </header>
   );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,11 @@
 import { cn } from '~/utils/utils';
 
 import SidebarGenres from './SidebarGenres';
-import SidebarLinks from './SidebarLinks';
 
 type Props = { className?: string };
 export default function Sidebar({ className }: Props) {
   return (
     <nav className={cn('space-y-8', className)}>
-      <SidebarLinks />
       <SidebarGenres />
     </nav>
   );

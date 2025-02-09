3b2ec7529446c8a0b8be33bd097e0fffc3296eb7
StrigZ <mirgasan200@gmail.com>
2025-02-09 22:39:03 +0300

    Fix loading spinner position

--- src/components/GameCardList.tsx
+++ src/components/GameCardList.tsx
@@ -7,23 +7,27 @@ import GameCard from './GameCard';
 
 type Props = { className?: string; games: Game[]; isLoading: boolean };
 export default function GameCardList({ className, games, isLoading }: Props) {
-  if (isLoading) return <Loader className="mx-auto animate-spin" size={128} />;
-
   return (
     <ul
       className={cn(
-        'grid w-full grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8',
+        'grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8',
         className,
       )}
     >
-      {games.map((game) => (
-        <li key={game.id}>
-          <GameCard
-            className="aspect-[16_/_9] shadow transition-transform hover:-translate-y-1"
-            {...game}
-          />
-        </li>
-      ))}
+      {isLoading ? (
+        <div className="absolute inset-0 flex h-full w-full items-center justify-center">
+          <Loader className="animate-spin" size={108} />
+        </div>
+      ) : (
+        games.map((game) => (
+          <li key={game.id}>
+            <GameCard
+              className="aspect-[16_/_9] shadow transition-transform hover:-translate-y-1"
+              {...game}
+            />
+          </li>
+        ))
+      )}
     </ul>
   );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -17,27 +17,35 @@ export default function Sidebar({
   isLoading,
   activeGenre,
 }: Props) {
-  if (isLoading) return <Loader className="mx-auto animate-spin" size={128} />;
-
   return (
-    <nav className={cn('space-y-4', className)}>
+    <nav className={cn('relative space-y-4', className)}>
       <h2 className="sticky top-0 bg-slate-950 p-4 pt-0 text-3xl font-bold shadow">
         Genres
       </h2>
-      <ul className="space-y-1">
-        {genres.map((genre) => (
-          <li key={genre.id} className="flex gap-4">
-            <button
-              className={cn(
-                'w-full rounded px-4 py-2 text-left text-lg transition-colors hover:bg-slate-600 active:scale-95',
-                { 'bg-slate-600': activeGenre === genre.slug },
-              )}
-              onClick={() => handleFilterChange('genres', genre.slug)}
-            >
-              {genre.name === 'Massively Multiplayer' ? 'MMO' : genre.name}
-            </button>
-          </li>
-        ))}
+      <ul
+        className={cn('space-y-1', {
+          'flex items-center justify-center': isLoading,
+        })}
+      >
+        {isLoading ? (
+          <div className="absolute inset-0 flex h-full w-full items-center justify-center">
+            <Loader className="animate-spin" size={108} />
+          </div>
+        ) : (
+          genres.map((genre) => (
+            <li key={genre.id} className="flex gap-4">
+              <button
+                className={cn(
+                  'w-full rounded px-4 py-2 text-left text-lg transition-colors hover:bg-slate-600 active:scale-95',
+                  { 'bg-slate-600': activeGenre === genre.slug },
+                )}
+                onClick={() => handleFilterChange('genres', genre.slug)}
+              >
+                {genre.name === 'Massively Multiplayer' ? 'MMO' : genre.name}
+              </button>
+            </li>
+          ))
+        )}
       </ul>
     </nav>
   );
